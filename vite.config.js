3acfc752c06c3540949391723336bba189fd0a83
BebeDol16 <rabemanandazadolin16@gmail.com>
2026-08-24 09:06:33 +0300

    Ajouter vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const rootDir = fileURLToPath(new URL(".", import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        boutique: resolve(rootDir, "index.html"),
+        connexion: resolve(rootDir, "login.html"),
+        compte: resolve(rootDir, "account.html"),
+        administration: resolve(rootDir, "admin.html")
+      }
+    }
+  }
+});
+
